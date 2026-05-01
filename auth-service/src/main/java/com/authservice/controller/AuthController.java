8898d2011874948212db65857a78528e07945e36
mayuri <mayuri1891@gmail.com>
2026-05-01 22:32:38 +0530

    added api-gateway

--- auth-service/src/main/java/com/authservice/controller/AuthController.java
+++ auth-service/src/main/java/com/authservice/controller/AuthController.java
@@ -5,7 +5,9 @@ import com.authservice.dto.AuthResponse;
 import com.authservice.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import java.util.List;
 @lombok.extern.slf4j.Slf4j
 @RestController
 @RequestMapping("/auth")
@@ -18,11 +20,21 @@ public class AuthController {
 
     @PostMapping("/login")
     public AuthResponse login(@RequestBody AuthRequest request) {
-    log.info("Received login request for username 1: {}", request.getUsername());
+
         String token = authService.authenticate(
                 request.getUsername(),
                 request.getPassword()
         );
+
+        // ✅ VERY IMPORTANT
+        UsernamePasswordAuthenticationToken authentication =
+                new UsernamePasswordAuthenticationToken(
+                        request.getUsername(),
+                        null,
+                        List.of() // roles if needed
+                );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
 
         return new AuthResponse(token);
     }
