caabe8a0c1297e50da840393232da7e7163749c3
mayuri <mayuri1891@gmail.com>
2026-05-05 05:00:39 +0530

    added api-gateway

--- auth-service/src/main/java/com/authservice/controller/AuthController.java
+++ auth-service/src/main/java/com/authservice/controller/AuthController.java
@@ -10,7 +10,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import java.util.List;
 @lombok.extern.slf4j.Slf4j
 @RestController
-@RequestMapping("/auth")
+//@RequestMapping("/auth")
 public class AuthController {
 
     @Autowired
